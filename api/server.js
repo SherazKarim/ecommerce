6367fce6356e34fb7f6f989c791a1819e9244cd7
SherazKarim <sherazkarimz@gmail.com>
2024-03-15 13:40:17 +0500

    finishing touch

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors({ origin: "*", credentials: true }))
 app.use('/api/product', productRoute)
 app.use('/api/auth', userRoute)
 app.use("/api/orders",ordersRoute)
