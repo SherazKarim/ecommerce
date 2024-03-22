69270b2022a0446049c284482ff7ea76ff6da4c2
SherazKarim <sherazkarimz@gmail.com>
2024-03-22 11:26:21 +0500

    fixed:change frontEnd Url

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ connect();
 //     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
 //   }));
 const corsOptions = {
-    origin: "https://ecom-app-drab.vercel.app",
+    origin: "https://ecommerce-76ha.vercel.app",
     credentials: true,
     optionsSuccessStatus: 200,
 };
@@ -34,7 +34,7 @@ app.use("/api/orders", ordersRoute)
 app.get("/", (req, res) => {
     res.json("Hello")
 })
-const YOUR_DOMAIN = 'https://ecom-app-drab.vercel.app';
+const YOUR_DOMAIN = 'https://ecommerce-76ha.vercel.app';
 const stripeClient = stripe(process.env.STRIPE_SECRET_KEY);
 
 app.post('/api/order/create-checkout-session', async (req, res) => {
