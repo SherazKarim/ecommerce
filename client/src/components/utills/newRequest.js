0f5efeced57ee82061039ec13c2307d8f8bc1256
SherazKarim <sherazkarimz@gmail.com>
2024-03-29 22:11:47 +0500

    fixed : backend connection

--- client/src/components/utills/newRequest.js
+++ client/src/components/utills/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-    // baseURL: "https://ecommerce-production-8fac.up.railway.app/api/",
-    baseURL: "http://localhost:8000/api/",
+    baseURL: "https://ecommerce-production-8fac.up.railway.app/api/",
+    // baseURL: "http://localhost:8000/api/",
     withCredentials:true
 })
